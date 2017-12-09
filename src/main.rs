53be9815c6de1e0d5d46c8dbcfd39d96e7864cad
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-09 17:04:47 +0900

    Implement interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate docopt;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+USAGE:
+    docbase-cli
+    docbase-cli config
+    docbase-cli post <post-file-path>...
+    docbase-cli (help | --version)
+
+Options:
+    -h, --help      Show this screen
+    -v, --version   Show version
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    cmd_config: bool,
+    cmd_post: bool,
+    arg_post_file_repo: Vec<String>,
+    flag_help: bool,
+    flag_version: bool
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE).and_then(|d| d.deserialize()).unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 }
